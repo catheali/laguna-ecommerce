08a3aafb83167989998b278705c0e420ec154ee3
catheali <aliciacatherinealencar@gmail.com>
2024-04-10 16:56:27 -0300

    style:  add navbar tailwind styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
 	<div>
-		<header>
-			<nav>
-				<NuxtLink to="/">Laguna</NuxtLink>
-				<ul>
+		<header class="shadow-sm bg-white">
+			<nav class="container mx-auto p-4 flex justify-between">
+				<NuxtLink to="/" class="font-bold">Laguna</NuxtLink>
+				<ul class="flex gap-4">
 					<li><NuxtLink to="/">Home</NuxtLink></li>
 					<li><NuxtLink to="/about">About</NuxtLink></li>
 					<li><NuxtLink to="/products">Products</NuxtLink></li>
 				</ul>
 			</nav>
 	    </header>
-		<div>
+		<div class="container mx-auto p-4">
 			<slot />
 		</div>
 	</div>
