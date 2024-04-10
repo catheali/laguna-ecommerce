239fee76ab44763fd7f57b66bc5837562095fa26
catheali <aliciacatherinealencar@gmail.com>
2024-04-10 15:30:35 -0300

    feat: add About page

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+	  <h2>About</h2>
+	  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores magnam similique numquam explicabo eaque ut nostrum cumque labore officia dolores eum quis, omnis deserunt rem? Ullam consequatur beatae sunt omnis?</p>
+	  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores magnam similique numquam explicabo eaque ut nostrum cumque labore officia dolores eum quis, omnis deserunt rem? Ullam consequatur beatae sunt omnis?</p>
+	  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores magnam similique numquam explicabo eaque ut nostrum cumque labore officia dolores eum quis, omnis deserunt rem? Ullam consequatur beatae sunt omnis?</p>
+	</div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+	  h2 {
+		  margin-bottom: 2px;
+		  font-size: 36px;
+	  }
+  
+	  p {
+		  margin: 20px 0;
+	  }
+  </style>
