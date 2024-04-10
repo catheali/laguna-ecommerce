ce6bf5063ce276ed3642ddbed4f80cbbe00f7041
catheali <aliciacatherinealencar@gmail.com>
2024-04-10 15:30:07 -0300

    feat: add Default layout

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<header>
+			<nav>
+				<NuxtLink to="/">Laguna</NuxtLink>
+				<ul>
+					<li><NuxtLink to="/">Home</NuxtLink></li>
+					<li><NuxtLink to="/about">About</NuxtLink></li>
+					<li><NuxtLink to="/products">Products</NuxtLink></li>
+				</ul>
+			</nav>
+	    </header>
+		<div>
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+	.router-link-exact-active {
+		color: #333;
+	}
+
+</style>
