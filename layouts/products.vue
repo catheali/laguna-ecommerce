7ad74386d9a68f551fa51fb903dceabbc112bea4
catheali <aliciacatherinealencar@gmail.com>
2024-04-10 15:39:58 -0300

    feat: add Products layout

---
+++ layouts/products.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<header>
+			<nav>
+				<NuxtLink to="/products">Laguna Products</NuxtLink>
+			</nav>
+		</header>
+		<div>
+			<slot />
+		</div>
+		<footer>
+			<ul>
+				<li><NuxtLink to="/">Home</NuxtLink></li>
+				<li><NuxtLink to="/about">About</NuxtLink></li>
+				<li><NuxtLink to="/products">Products</NuxtLink></li>
+			</ul>
+	    </footer>
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+	.router-link-exact-active {
+		color: #333;
+	}
+
+</style>
