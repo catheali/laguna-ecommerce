b37ebacddf043adf00cae150005be774a11d2af3
catheali <aliciacatherinealencar@gmail.com>
2024-04-11 16:29:51 -0300

    build: add material-icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,20 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  app:{
+	head:{
+		title: 'Laguna',
+		meta: [
+			{name: 'description',
+			 content: 'example project of a ecommerce in Nuxt 3'
+			}
+		],
+		link: [
+			{
+				rel:'stylesheet', href:'https://fonts.googleapis.com/icon?family=Material+Icons'
+			}
+		]
+	}
+  }
 })
