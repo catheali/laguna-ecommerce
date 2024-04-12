4a8efbb7e17ee4f6977dd064ca86074cf7f091f1
catheali <aliciacatherinealencar@gmail.com>
2024-04-12 12:45:37 -0300

    docs: add comment about fetching with api keys

--- server/api/products.js
+++ server/api/products.js
@@ -6,6 +6,10 @@ export default defineEventHandler(async (event)=>{
 	// handle post data
 	const { age } = await readBody(event)
 
+	//api call with private key
+	// is recommended to use $fetch when we're using server route 
+	const {data} = await $fetch('url+apikey')
+	// it would not show in front end
 	return{
 		message: `Hello ${name}! You are ${age} years old.`
 	} 
