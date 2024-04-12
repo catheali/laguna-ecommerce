7cecdf4a2050927d2a3363c2f14beaa2a62394ae
catheali <aliciacatherinealencar@gmail.com>
2024-04-12 14:46:38 -0300

    chore: add api key to currency API

--- server/api/products.js
+++ server/api/products.js
@@ -7,8 +7,9 @@ export default defineEventHandler(async (event)=>{
 	const { age } = await readBody(event)
 
 	//api call with private key
+	const {currencyKey} = useRuntimeConfig()
 	// is recommended to use $fetch when we're using server route 
-	const {data} = await $fetch('url+apikey')
+	const {data} = await $fetch('https://api.currencyapi.com/v3/latest?apikey='+currencyKey)
 	// it would not show in front end
 	return{
 		message: `Hello ${name}! You are ${age} years old.`
